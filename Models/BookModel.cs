5cf5533297017ecc700afc50891e6a9b0f974dd7
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-08-30 16:27:35 +0545

    Implement Required model validation

--- Models/BookModel.cs
+++ Models/BookModel.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace BookStore.API.Models
 {
     public class BookModel
     {
-        public int Id {get; set;}
-        public string Title {get; set;}
-        public string Description {get; set;}
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please add title property")]
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 }
